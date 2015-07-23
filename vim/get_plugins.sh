cbcc80b24ed02ade5ffc462debf0b98bc8500e85
Frans Slothouber <frans.slothouber@intraffic.nl>
2015-07-23 15:46:24 +0200

    Add vim stuff

---
+++ vim/get_plugins.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+git clone https://github.com/easymotion/vim-easymotion
+git clone git://github.com/tpope/vim-fugitive.git
+git clone https://github.com/bling/vim-airline
+git clone https://github.com/scrooloose/nerdtree.git
+git clone git://github.com/altercation/vim-colors-solarized.git
+
