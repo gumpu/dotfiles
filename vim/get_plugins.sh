05fc14b329e50ba79a7d948c63e1dcc15acf18c3
Frans Slothouber <rfsber@xs4all.nl>
2015-07-26 17:48:23 +0200

    Installation bug fix

--- vim/get_plugins.sh
+++ vim/get_plugins.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
 git clone https://github.com/easymotion/vim-easymotion
-git clone git://github.com/tpope/vim-fugitive.git
+git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/bling/vim-airline
 git clone https://github.com/scrooloose/nerdtree.git
-git clone git://github.com/altercation/vim-colors-solarized.git
+git clone https://github.com/altercation/vim-colors-solarized.git
 
 # Vim-R-Plugin
 # Also need vimcom_1.2-6.tar.gz 
-git clone git@github.com:vim-scripts/Vim-R-plugin.git
+# git clone git@github.com:vim-scripts/Vim-R-plugin.git
 
 
