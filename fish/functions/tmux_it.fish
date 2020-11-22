e65f1f9b0218811793e080a287a842cdbb744867
Frans Slothouber <frans.slothouber@gmail.com>
2020-11-22 15:13:00 +0100

    Adds more tmux environments

--- fish/functions/tmux_it.fish
+++ fish/functions/tmux_it.fish
@@ -7,7 +7,7 @@ function tmux_it
     case "stm"
         begin
             echo 'STM'
-            cd /home/frans/Documents/InTraffic/P44/Sources/
+            cd /home/frans/Documents/InTraffic/P44/Source/
             # Start in detached mode otherwise the commands
             # that are followed are only executed after
             # the session ends :)
@@ -25,7 +25,27 @@ function tmux_it
             tmux new-window -t "$argv" 'mutt'
             tmux attach -t "$argv"
         end
+    case "static"
+        begin
+            echo 'Static'
+            cd /home/frans/Documents/Personal/StaticWeb/
+            tmux new-session -d -s "$argv"
+            tmux new-window -t "$argv" -n 'Edit'
+            tmux new-window -t "$argv" -n 'Build'
+            tmux new-window -t "$argv" -n 'Serve'
+            tmux attach -t "$argv"
+        end
     end
+    case "euler"
+        begin
+            echo 'Euler'
+            cd /home/frans/Documents/Personal/Euler/projecteuler
+            tmux new-session -d -s "$argv"
+            tmux new-window -t "$argv" -n 'Edit'
+            tmux new-window -t "$argv" -n 'Debug'
+            tmux new-window -t "$argv" -n 'Build'
+            tmux attach -t "$argv"
+        end
     echo 'Done'
 end
 
