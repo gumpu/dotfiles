e3934475f14c5273d63ee048a74fd450f136477d
Frans Slothouber <Frans.Slothouber@NS.NL>
2022-11-09 23:14:33 +0100

    Adds lightline

--- vim/get_plugins.sh
+++ vim/get_plugins.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# git clone https://github.com/easymotion/vim-easymotion
-# git clone https://github.com/scrooloose/nerdtree.git
-# git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
-# git clone https://github.com/davidhalter/jedi-vim.git
-
+# Pairs of handy bracket mappings
 git clone https://github.com/tpope/vim-unimpaired.git
 
 # Use :A to got from the .c file to the .h file and visa versa
 git clone https://github.com/vim-scripts/a.vim.git
+
 # The one and only true color scheme
 git clone https://github.com/altercation/vim-colors-solarized.git
 
@@ -17,9 +14,17 @@ git clone https://github.com/vim-airline/vim-airline-themes.git
 
 # Search for and load local vimrc files
 git clone https://github.com/embear/vim-localvimrc.git
+
+#
 git clone https://github.com/jmcantrell/vim-virtualenv
+
+# vim interface the the ack command.
 git clone https://github.com/mileszs/ack.vim.git
+
+# Git TUI in vim, so good it should be illegal
 git clone https://github.com/tpope/vim-fugitive.git
+
+#
 git clone https://github.com/vim-scripts/taglist.vim.git
 
 # Syntax highlighting for GNU Assembler
@@ -31,14 +36,25 @@ git clone https://github.com/tomtom/tlib_vim.git
 git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
 git clone https://github.com/garbas/vim-snipmate.git
 # ----------------------------------------------------------------------------
-
 # Syntax highlighting for MiniZinc files
 git clone git@github.com:paolodragone/vim-pymzn.git
 
-# ----------------------------------------------------------------------------
+
+
+# Syntax highlighting for the fish shell
 git clone git@github.com:dag/vim-fish.git
+
+# Send lines from your source file to be executed by some interpretor in another
+# window.  Makes vim an ideal R gui.
+git clone https://github.com/jpalardy/vim-slime
+# ----------------------------------------------------------------------------
 
 # Testing
 git clone https://github.com/lfv89/vim-interestingwords
-git clone https://github.com/jpalardy/vim-slime
+
+git clone https://github.com/tpope/vim-surround
+git clone https://github.com/airblade/vim-gitgutter
+
+git clone https://github.com/itchyny/lightline.vim
+
 # ------------------------ End of file -----------------------------
